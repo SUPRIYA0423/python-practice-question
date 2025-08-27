6e4c6baf6fe1fbe500e4197f804beb9f5981a306
SUPRIYA0423 <supriuuu.04@gmail.com>
2025-08-27 23:01:16 +0530

    Add files via upload

---
+++ range.py/range1.py
@@ -0,0 +1,23 @@
+#print(list(range(0,6)))
+#syntax of for loop
+#for i in range(1,10,2):
+#    print(i)
+#for i in range(1,10,2):
+#    print(i*i,end=" ")
+#print all integers from 1 to N
+#for i in range(1,11,1):
+#    print(i)
+#print all odd integers from 1 to N
+#N=10
+#for i in range(1,N+1,2):
+#    print(i,end=" ")
+# power
+#A=3
+#B=2
+#ans=1
+#for i in range(B):
+#    ans=ans*A
+#    print(ans,end=" ")
+#
+#
+
