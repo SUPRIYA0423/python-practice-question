2695fe2587ac5a83c99b403af9aea68c4c979477
SUPRIYA0423 <supriuuu.04@gmail.com>
2025-11-13 13:17:08 +0530

    Delete factorial.py

--- factorial.py
+++
@@ -1,8 +0,0 @@
-#factorial program using recursion
-def factorial(n):
-    if n == 0 or n == 1:
-        return 1  
-    else:
-        return n * factorial(n - 1) 
-n=int(input())
-print(factorial(n))
